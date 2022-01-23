efe57447017dc67b84cca6088e6e64a8fb9ed7cd
Kristen <kristenwood123@gmail.com>
2022-01-22 20:29:15 -0800

    setup express server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const http = require("http");
+const app = require("./src/app");
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server running at port ${3000}`);
+});
+
 // json-server --watch db.json
---
+++ src/app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const data = require("../utils/db.json");
+const app = express();
+
+module.exports = app;
