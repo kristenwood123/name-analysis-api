95a1d83d6de5af7c38afe76c9e5320a97b74c6fd
Kristen <kristenwood123@gmail.com>
2022-01-27 09:49:14 -0800

    added styling to response

--- src/app.js
+++ src/app.js
@@ -19,45 +19,51 @@ app.get("/", (req, res) => {
 
 app.get("/api/:firstName/:gender/:age", (req, res) => {
   try {
-    
     const { firstName, gender, age } = req.params;
     let sumOfName = 0;
-  
+
     // Name Validation
     const nameError = validateName(firstName).error;
     if (nameError) {
       return res.status(400).send({ nameError });
     }
-  
+
     // Gender Validation
     const genderError = validateGender(gender).error;
     if (genderError) {
       return res.status(400).send({ genderError });
     }
-  
+
     // Age Validation
     const ageError = validateAge(age).error;
     if (ageError) {
       return res.status(400).send({ ageError });
     }
-  
+
     for (let i = 0; i < firstName.length; i++) {
       let current = firstName[i];
       sumOfName += letters[current];
     }
-  
+
     // Name Analysis
     let nameAnalysis = data.users.find((desc) => desc.id === sumOfName)[gender];
-  
+
     if (age <= 50) {
       nameAnalysis = nameAnalysis["description1"];
     } else {
       nameAnalysis = nameAnalysis["description2"];
     }
-  
-    res.status(200).send({ analysis: nameAnalysis });
+
+    const resp = `
+    <h2>Hurray! Your result arrived</h2>
+    <hr>
+    <h4>Your name is ${firstName}, you are a ${gender} and currently ${age} years old <h4> 
+    <br> 
+    Name Analysis: <p>${nameAnalysis}</p>`;
+    //res.status(200).send({ analysis: nameAnalysis });
+    res.status(200).send(resp);
   } catch (error) {
-   res.status(500).send('Error analysing the data::', error) 
+    res.status(500).send("Error analysing the data::", error);
   }
 });
 
